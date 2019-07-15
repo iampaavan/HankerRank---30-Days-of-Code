9aa6e38e41d73bb195f07efe5f9e08452c5adae4
iampaavan <46580953+iampaavan@users.noreply.github.com>
2019-07-15 12:51:04 -0700

    Day-25

---
+++ Day-25.py
@@ -0,0 +1,31 @@
+def is_prime_faster(n):
+    """
+    Assumes that n is a positive natural number
+    """
+    # We know 1 is not a prime number
+    if n == 1:
+        return False
+
+    i = 2
+    # This will loop from 2 to int(sqrt(x))
+    while i*i <= n:
+        # Check if i divides x without leaving a remainder
+        if n % i == 0:
+            # This means that n has a factor in between 2 and sqrt(n)
+            # So it is not a prime number
+            return False
+        i += 1
+    # If we did not find any factor in the above loop,
+    # then n is a prime number
+    return True
+
+
+T = int(input())
+
+for i in range(T):
+    number = int(input())
+
+    if number >= 2 and is_prime_faster(number):
+        print('Prime')
+    else:
+        print('Not prime')
